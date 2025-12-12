8750f0c3284529c238683619acdbbe58b736b426
PDTnhah <phamduythanh43@gmail.com>
2025-12-12 07:48:35 +0700

    feat: update mqtt config

--- backend/src/config/mqtt.js
+++ backend/src/config/mqtt.js
@@ -1,8 +1,8 @@
 const mqtt = {
-    url: "mqtts://28f4948412341f9a4480874c05255d3.s1.eu.hivemq.cloud:8883",
+    url: "mqtts://28f49484412341f9a4480874c05255d3.s1.eu.hivemq.cloud:8883",
     options: {
-        username: "Pham_Duy_Thanh_20225158",
-        password: "Pham_Duy_Thanh_20225158",
+        username: "Admin123",
+        password: "Admin123",
         clientId: `server_backend_${Math.random().toString(16).slice(3)}`,
         clean: true, // Xóa session cũ khi connect lại
         connectTimeout: 4000,
